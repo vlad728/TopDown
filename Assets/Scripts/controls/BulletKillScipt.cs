88cce6ed118964f3fe9cf6a6a17552f933f00ac4
vlad728 <yulia.bogunova2017@gmail.com>
2026-01-24 11:40:46 -0800

    Bullet final product

---
+++ Assets/Scripts/controls/BulletKillScipt.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class BulletKillScipt : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/controls/ShootControls.cs
+++ Assets/Scripts/controls/ShootControls.cs
@@ -1,9 +1,12 @@
+using System.Collections;
 using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class ShootControls : MonoBehaviour
 {
     public Transform firePoint;
+    public GameObject bulletPrefab;
+    private float bulletVel = 20;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -23,15 +26,39 @@ public class ShootControls : MonoBehaviour
     private void Shoot()
     {
         RaycastHit hit;
+        Vector3 targetPoint;
         if (Physics.Raycast(firePoint.position, firePoint.forward, out hit, 50))
         {
-            if (hit.collider.CompareTag("Enemy"))
-            {
-                Destroy(hit.collider.gameObject);
-            }
-
+            //if (hit.collider.CompareTag("Enemy"))
+            //{
+            //    Destroy(hit.collider.gameObject);
+            //}
+            targetPoint = hit.point;
+        }
+        else
+        {
+            targetPoint = firePoint.position + firePoint.forward * 50;
         }
         Debug.DrawRay(firePoint.position, firePoint.forward * 50, Color.green, 0.1f);
+        
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        StartCoroutine(MoveBullet(bullet.transform, firePoint.position, targetPoint));
     }
 
+    IEnumerator MoveBullet(Transform bulletTransform, Vector3 startPos, Vector3 endPos)
+    {
+        float distance = Vector3.Distance(startPos, endPos);
+        float lifeTime = distance / bulletVel;
+        float timer = 0;
+        while (timer < lifeTime && bulletTransform != null)
+        {
+            timer += Time.deltaTime;
+            bulletTransform.position = Vector3.Lerp(startPos, endPos, timer / lifeTime);
+            yield return null;
+        }
+        if (bulletTransform != null)
+        {
+            Destroy(bulletTransform.gameObject);
+        }
+    }
 }
